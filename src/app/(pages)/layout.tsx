2cce134e8bf3d70589e54fcd1c32adf7c65a82ee
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-11 15:56:10 -0700

    Simplify pages base layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
 import '@/styles/globals.css';
 
-export default function PagesRootLayout(props: PropsWithChildren) {
+const PagesRootLayout = (props: PropsWithChildren) => {
 	return (
 		<html lang='en'>
-			<body>
-				{props.children}
-				<div className='fixed bottom-4 z-20 p-2 bg-black text-white'><Link href='/'>Home</Link></div>
-			</body>
+			<body>{props.children}</body>
 		</html>
 	);
-}
+};
+
+export default PagesRootLayout;
