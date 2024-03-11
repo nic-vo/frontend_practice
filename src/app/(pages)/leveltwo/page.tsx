cebafef1b9a30f862ec1d738dbccefb0bbe0a4db
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-11 15:59:29 -0700

    Prettify leveltwo layout + convert to arrow func

--- src/app/(pages)/leveltwo/page.tsx
+++ src/app/(pages)/leveltwo/page.tsx
@@ -1,36 +1,39 @@
 import Link from 'next/link';
 import { getPages } from '@/lib/pages';
 
-const segment = 'leveltwo'
+const segment = 'leveltwo';
 
-export default async function LevelTwoIndex() {
+const LevelTwoIndex = async () => {
 	const pages = await getPages(['(pages)', segment]);
 
 	return (
 		<main>
 			<h1>Level Two Clones</h1>
-			<section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, deleniti quaerat impedit perspiciatis at quod amet laboriosam voluptatibus omnis eius fugit quia rem tenetur, atque nihil doloremque necessitatibus repellat. Nisi.</section>
-			{
-				pages.length === 0 ? <p>Nothing here!</p>
-					:
-					(
-						<nav>
-							<ul>
-								{
-									pages.map(link => (
-										<li key='link'>
-											<Link href={`/${segment}/${link}`}>{link}</Link>
-										</li>
-									))
-								}
-							</ul>
-						</nav>
-					)
-			}
+			<section>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
+				deleniti quaerat impedit perspiciatis at quod amet laboriosam
+				voluptatibus omnis eius fugit quia rem tenetur, atque nihil doloremque
+				necessitatibus repellat. Nisi.
+			</section>
+			{pages.length === 0 ? (
+				<p>Nothing here!</p>
+			) : (
+				<nav>
+					<ul>
+						{pages.map((link) => (
+							<li key='link'>
+								<Link href={`/${segment}/${link}`}>{link}</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+			)}
 		</main>
 	);
-}
+};
+
+export default LevelTwoIndex;
 
 export const metadata = {
-	title: 'Nic Vo | Level Two Clones'
+	title: 'Nic Vo | Level Two Clones',
 };
