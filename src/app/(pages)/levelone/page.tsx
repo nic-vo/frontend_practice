0de977bffbcae884941c31ff8df9ea1455a2dee5
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-11 15:58:01 -0700

    Prettify levelone layout + convert to arrow func

--- src/app/(pages)/levelone/page.tsx
+++ src/app/(pages)/levelone/page.tsx
@@ -1,36 +1,39 @@
 import Link from 'next/link';
 import { getPages } from '@/lib/pages';
 
-const segment = 'levelone'
+const segment = 'levelone';
 
-export default async function LevelOneIndex() {
+const LevelOneIndex = async () => {
 	const pages = await getPages(['(pages)', segment]);
 
 	return (
 		<main>
 			<h1>Level One Clones</h1>
-			<section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, deleniti quaerat impedit perspiciatis at quod amet laboriosam voluptatibus omnis eius fugit quia rem tenetur, atque nihil doloremque necessitatibus repellat. Nisi.</section>
-			{
-				pages.length === 0 ? <p>Nothing here!</p>
-					:
-					(
-						<nav>
-							<ul>
-								{
-									pages.map(link => (
-										<li key='link'>
-											<Link href={`/${segment}/${link}`}>{link}</Link>
-										</li>
-									))
-								}
-							</ul>
-						</nav>
-					)
-			}
+			<section>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
+				deleniti quaerat impedit perspiciatis at quod amet laboriosam
+				voluptatibus omnis eius fugit quia rem tenetur, atque nihil doloremque
+				necessitatibus repellat. Nisi.
+			</section>
+			{pages.length === 0 ? (
+				<p>Nothing here!</p>
+			) : (
+				<nav>
+					<ul>
+						{pages.map((link) => (
+							<li key='link'>
+								<Link href={`/${segment}/${link}`}>{link}</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+			)}
 		</main>
 	);
-}
+};
+
+export default LevelOneIndex;
 
 export const metadata = {
-	title: 'Nic Vo | Level One Clones'
+	title: 'Nic Vo | Level One Clones',
 };
