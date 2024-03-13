5497d9c1c3af46081c4e07b78c28ab8aff998b8c
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-12 23:28:52 -0700

    Change aside to imperatively allow tabbing if pane is toggled
    Technically should be okay because the UI is not re-rendered based on state

--- src/components/global/DualView.tsx
+++ src/components/global/DualView.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, PropsWithChildren } from 'react';
+import { useState, PropsWithChildren, useEffect, useRef } from 'react';
 import {
 	CloneMissing,
 	InfoMissing,
@@ -10,12 +10,30 @@ import {
 
 const ToggleAside = (props: PropsWithChildren) => {
 	const [toggled, setToggled] = useState(true);
+
+	const asideRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		if (!asideRef.current) return;
+		const anchors = asideRef.current.querySelectorAll('a');
+		if (!anchors) return;
+		if (toggled) {
+			anchors.forEach((anchor) => (anchor.tabIndex = -1));
+		} else {
+			anchors.forEach((anchor) => (anchor.tabIndex = 0));
+			const heading = asideRef.current.querySelector('h2');
+			if (heading) heading.focus();
+		}
+	}, [toggled]);
+
 	return (
 		<aside
-			className={`fixed bottom-0 lg:bottom-2 lg:right-2 flex flex-col gap-2 bg-black text-white w-full h-full lg:max-w-[33svw] max-h-[50svh] z-10 transition-transform ${toggled ? 'translate-x-full lg:translate-x-[calc(100%_+_0.75rem)]' : ''} rounded-t-3xl lg:rounded-3xl`}>
+			ref={asideRef}
+			className={`fixed bottom-0 lg:bottom-2 lg:right-2 flex flex-col gap-2 bg-black text-white w-full h-full lg:max-w-[33svw] max-h-[50svh] z-10 transition-transform ${toggled ? 'translate-x-full lg:translate-x-[calc(100%_+_0.75rem)]' : ''} rounded-t-3xl lg:rounded-3xl text-lg p-8 gap-8 flex flex-col z-50`}
+			onBlur={() => setToggled(false)}>
 			<button
 				onClick={() => setToggled(!toggled)}
-				className={`flex gap-2 ${toggled ? 'before:content-["<<"]' : 'after:content-[">>"]'} absolute translate-y-[-110%] lg:translate-y-0 ${!toggled ? 'translate-x-2 lg:translate-x-[-110%]' : 'translate-y-0 translate-x-[-110%]'} bg-rose-500 p-4 transition-transform rounded-lg`}>
+				className={`flex gap-2 ${toggled ? 'before:content-["<<"]' : 'after:content-[">>"]'} absolute translate-y-[-110%] lg:translate-y-0 ${!toggled ? 'translate-x-2 lg:translate-x-[-130%]' : 'translate-y-0 translate-x-[-130%]'} bg-rose-500 p-4 transition-transform rounded-lg`}>
 				{!toggled ? 'Close' : 'Open'} info
 			</button>
 			{props.children}
