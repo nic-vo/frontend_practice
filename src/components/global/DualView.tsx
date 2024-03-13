7bcedb5c52c2dc7658b60147422748a1bae279d7
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-12 18:25:12 -0700

    Move all server-able components to the correct file
    Init the ToggleAside component

--- src/components/global/DualView.tsx
+++ src/components/global/DualView.tsx
@@ -1,18 +1,25 @@
 'use client';
 
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState, PropsWithChildren } from 'react';
+import {
+	CloneMissing,
+	InfoMissing,
+	InfoNavMissing,
+	InfoTitleMissing,
+} from './DualViewServer';
 
-export const OriginalNav = (props: { original: string }) => {
-	return <Link href={props.original}>Original</Link>;
-};
-
-export const LayoutNav = (props: { upOne: string }) => {
+const ToggleAside = (props: PropsWithChildren) => {
+	const [toggled, setToggled] = useState(true);
 	return (
-		<>
-			<Link href={props.upOne}>Back</Link>
-			<Link href='/'>Home</Link>
-		</>
+		<aside
+			className={`fixed bottom-0 lg:bottom-2 lg:right-2 flex flex-col gap-2 bg-black text-white w-full h-full lg:max-w-[33svw] max-h-[50svh] z-10 transition-transform ${toggled ? 'translate-x-full lg:translate-x-[calc(100%_+_0.75rem)]' : ''} rounded-t-3xl lg:rounded-3xl`}>
+			<button
+				onClick={() => setToggled(!toggled)}
+				className={`flex gap-2 ${toggled ? 'before:content-["<<"]' : 'after:content-[">>"]'} absolute translate-y-[-110%] lg:translate-y-0 ${!toggled ? 'translate-x-2 lg:translate-x-[-110%]' : 'translate-y-0 translate-x-[-110%]'} bg-rose-500 p-4 transition-transform rounded-lg`}>
+				{!toggled ? 'Close' : 'Open'} info
+			</button>
+			{props.children}
+		</aside>
 	);
 };
 
