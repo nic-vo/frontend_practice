b619e1242f8cc43029f284475dffee0da10adebb
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-12 18:26:41 -0700

    Add aside toggle to layout
    Add conditional rendering of all parallel routes just in case

--- src/components/global/DualView.tsx
+++ src/components/global/DualView.tsx
@@ -27,24 +27,28 @@ const DualView = (props: {
 	clone: React.ReactNode;
 	info: React.ReactNode;
 	infonav: React.ReactNode;
+	infotitle: React.ReactNode;
 }) => {
-	const [toggled, setToggled] = useState(true);
-	const { clone, info, infonav } = props;
+	const { clone, info, infonav, infotitle } = props;
 	return (
 		<>
-			{clone}
-			<aside
-				className={`fixed bottom-0 lg:bottom-2 lg:right-2 flex flex-col gap-2 bg-black text-white rounded-lg w-full h-full lg:max-w-[33svw] max-h-[50svh] z-10 transition-transform ${!toggled ? 'translate-x-[calc(100%_+_0.75rem)]' : ''}`}>
-				<div className='p-4 overflow-auto'>
-					<nav className='flex gap-2 underline'>{infonav}</nav>
-					{info}
+			{clone || CloneMissing()}
+			<ToggleAside>
+				<div className='flex flex-col gap-4 p-8 h-full text-lg'>
+					<section className='overflow-hidden flex flex-col gap-4'>
+						{infotitle || InfoTitleMissing()}
+						<div className='flex flex-col gap-4 overflow-y-auto pr-4 font-light'>
+							{info || InfoMissing()}
+						</div>
+					</section>
+					<nav className='flex gap-2 font-light'>
+						<h2>Links:</h2>
+						<ul className='flex gap-2 underline'>
+							{infonav || InfoNavMissing()}
+						</ul>
+					</nav>
 				</div>
-				<button
-					onClick={() => setToggled(!toggled)}
-					className='absolute translate-x-[-110%] bg-red-300 p-4'>
-					Info
-				</button>
-			</aside>
+			</ToggleAside>
 		</>
 	);
 };
