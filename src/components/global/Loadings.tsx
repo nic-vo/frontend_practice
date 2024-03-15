96399fa71f76b0bfb041ebc6b167e9d9bb5eb805
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-15 02:10:44 -0700

    Center and enlarge page loader

--- src/components/global/Loadings.tsx
+++ src/components/global/Loadings.tsx
@@ -1,7 +1,7 @@
 export const DefaultPageLoader = () => {
 	return (
-		<main className='flex flex-col justify-items-center h-svh w-svw'>
-			<h1 className='block 4xl text-center'>Loading...</h1>
+		<main className='flex flex-col justify-items-center items-center h-svh w-svw'>
+			<h1 className='block text-4xl text-center'>Loading...</h1>
 		</main>
 	);
 };
