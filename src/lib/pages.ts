367ce8adaee79dab65601f7766efa6a216c4523f
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-15 01:10:49 -0700

    Remove nesting and filter for .ico and parallel routes

--- src/lib/pages.ts
+++ src/lib/pages.ts
@@ -3,17 +3,16 @@ import path from 'path';
 
 export async function getPages(segments: string[]) {
 	try {
-		try {
-			const cwd = process.cwd();
-			const base = ['src'];
-			const dir = await fs.readdir(path.join(cwd, ...base, ...segments));
-			return dir.filter((link) => {
-				return /.tsx$/.test(link) === false && /^@/.test(link) === false;
-			});
-		} catch (e: any) {
-			console.log(e);
-			return [];
-		}
+		const cwd = process.cwd();
+		const base = ['src'];
+		const dir = await fs.readdir(path.join(cwd, ...base, ...segments));
+		return dir.filter((link) => {
+			return (
+				/.tsx$/.test(link) === false &&
+				/^@/.test(link) === false &&
+				/.ico$/.test(link) === false
+			);
+		});
 	} catch (e: any) {
 		console.error(e);
 		return [];
