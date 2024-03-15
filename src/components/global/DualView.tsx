eed6719dc57bdc4075c547ba83e12c13ac37f3a5
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-15 01:40:49 -0700

    Remove nesting in DualView

--- src/components/global/DualView.tsx
+++ src/components/global/DualView.tsx
@@ -37,57 +37,55 @@ const ToggleAside = (props: PropsWithChildren) => {
 	}, [toggled]);
 
 	return (
-		<>
-			<aside
-				className={`fixed bottom-0 right-0 lg:bottom-2 lg:right-2 flex flex-col gap-2 bg-black text-white h-full w-full max-w-prose lg:max-w-[33svw] max-h-[50svh] lg:max-h-[75svh] z-10 transition-all ${toggled ? 'translate-x-full lg:translate-x-[calc(100%+2rem)] shadow-none' : 'translate-x-0 shadow-2xl'} rounded-t-3xl lg:rounded-3xl text-base lg:text-lg p-8 gap-8 flex flex-col z-50`}>
-				<nav className='fixed -translate-x-full lg:-translate-x-[calc(100%+2rem)] bottom-2 -left-2'>
-					<ul className='flex flex-col-reverse gap-4 text-2xl'>
-						<li>
-							<button
-								onClick={() => setToggled(!toggled)}
-								id='toggleInfoPanel'
-								className={toggleClass}>
-								<FaInfoCircle />
-								<span className={interactiveLook.hiddenAccessibleText}>
-									{!toggled ? 'Close' : 'Open'} info panel
-								</span>
-							</button>
-						</li>
-						<li>
-							<Link
-								href={`/${pathSegments[0]}`}
-								className={toggleClass}>
-								<FaList />
-								<span className={interactiveLook.hiddenAccessibleText}>
-									Back to index
-								</span>
-							</Link>
-						</li>
-						<li>
-							<Link
-								href='/'
-								className={toggleClass}>
-								<FaHome />
-								<span className={interactiveLook.hiddenAccessibleText}>
-									Back to home
-								</span>
-							</Link>
-						</li>
-					</ul>
-				</nav>
-				<div
-					className='flex flex-col gap-4 overflow-hidden h-full w-full'
-					ref={divRef}>
-					{props.children}
-				</div>
-				<button
-					onClick={() => setToggled(true)}
-					className='absolute top-4 right-4 p-2 border-2 rounded-full hover:text-black hover:bg-white focus:text-black focus:bg-white transition-all'
-					tabIndex={toggled ? -1 : 0}>
-					<FaTimes />
-				</button>
-			</aside>
-		</>
+		<aside
+			className={`fixed bottom-0 right-0 lg:bottom-2 lg:right-2 flex flex-col gap-2 bg-black text-white h-full w-full max-w-prose lg:max-w-[33svw] max-h-[50svh] lg:max-h-[75svh] z-10 transition-all ${toggled ? 'translate-x-full lg:translate-x-[calc(100%+2rem)] shadow-none' : 'translate-x-0 shadow-2xl'} rounded-t-3xl lg:rounded-3xl text-base lg:text-lg p-8 gap-8 flex flex-col z-50`}>
+			<nav className='fixed -translate-x-full lg:-translate-x-[calc(100%+2rem)] bottom-2 -left-2'>
+				<ul className='flex flex-col-reverse gap-4 text-2xl'>
+					<li>
+						<button
+							onClick={() => setToggled(!toggled)}
+							id='toggleInfoPanel'
+							className={toggleClass}>
+							<FaInfoCircle />
+							<span className={interactiveLook.hiddenAccessibleText}>
+								{!toggled ? 'Close' : 'Open'} info panel
+							</span>
+						</button>
+					</li>
+					<li>
+						<Link
+							href={`/${pathSegments[0]}`}
+							className={toggleClass}>
+							<FaList />
+							<span className={interactiveLook.hiddenAccessibleText}>
+								Back to index
+							</span>
+						</Link>
+					</li>
+					<li>
+						<Link
+							href='/'
+							className={toggleClass}>
+							<FaHome />
+							<span className={interactiveLook.hiddenAccessibleText}>
+								Back to home
+							</span>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			<div
+				className='flex flex-col gap-4 overflow-hidden h-full w-full'
+				ref={divRef}>
+				{props.children}
+			</div>
+			<button
+				onClick={() => setToggled(true)}
+				className='absolute top-4 right-4 p-2 border-2 rounded-full hover:text-black hover:bg-white focus:text-black focus:bg-white transition-all'
+				tabIndex={toggled ? -1 : 0}>
+				<FaTimes />
+			</button>
+		</aside>
 	);
 };
 
