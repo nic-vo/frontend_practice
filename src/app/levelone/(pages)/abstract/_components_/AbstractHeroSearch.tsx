51cc756031674996bac57e938bf14ca83c21d387
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-08-13 13:36:18 -0700

    Move search logic to a hook instead of context
    So it can be imported into the two search components

--- src/app/levelone/(pages)/abstract/_components_/AbstractHeroSearch.tsx
+++ src/app/levelone/(pages)/abstract/_components_/AbstractHeroSearch.tsx
@@ -1,31 +1,16 @@
 'use client';
 
-import { ChangeEvent, useContext, useState } from 'react';
-import AbstractSearchContext from './AbstractSearch';
 import { IoArrowForward } from 'react-icons/io5';
 
 import interactiveLook from '@/styles/globals.module.scss';
 
 const AbstractHeroForm = () => {
-	const [current, setCurrent] = useState('');
-	const { setNewQuery } = useContext(AbstractSearchContext);
-
-	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) =>
-		setCurrent(e.target.value);
-
-	const onSubmitHandler = (e: React.FormEvent) => {
-		e.preventDefault();
-		setNewQuery(current);
-	};
-
 	return (
-		<form onSubmit={onSubmitHandler}>
+		<form>
 			<h2>How can we help?</h2>
 			<input
 				type='search'
-				name='querySingle'
 				required={true}
-				onChange={onChangeHandler}
 			/>
 			<button type='submit'>
 				<IoArrowForward />
@@ -36,3 +21,5 @@ const AbstractHeroForm = () => {
 		</form>
 	);
 };
+
+export default AbstractHeroForm;
