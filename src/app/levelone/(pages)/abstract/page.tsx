6ed64261b260b5d3606dfd9575edc6f610c03781
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-08-13 16:13:08 -0700

    Add Abstract Karla font and main components
    Style h1 too

--- src/app/levelone/(pages)/abstract/page.tsx
+++ src/app/levelone/(pages)/abstract/page.tsx
@@ -1,21 +1,22 @@
-import AbstractFooter from './_components_/AbstractFooter';
-import AbstractHeader from './_components_/AbstractHeader';
+import { karla } from '@/styles/fonts';
+import {
+	AbstractFooter,
+	AbstractHeader,
+	AbstractHeroSearch,
+} from './_components_';
 
 const AbstractHomepage = () => {
 	return (
-		<>
+		<div className={karla.className}>
 			<AbstractHeader />
 			<main>
-				<section>
-					<div>
-						<h2>How can we help?</h2>
-						{/* TODO: Search bar that redirects to appropriate URL */}
-					</div>
-				</section>
-				<div></div>
+				<div className='flex flex-col items-center py-32 bg-indigo-100 gap-8'>
+					<h1 className='text-3xl sm:text-6xl font-medium'>How can we help?</h1>
+					<AbstractHeroSearch />
+				</div>
 			</main>
 			<AbstractFooter />
-		</>
+		</div>
 	);
 };
 
