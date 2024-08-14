c76ecfc41535a9088f1f3372d11941ebee71039c
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-08-13 22:40:11 -0700

    Add Abstract Grid
    Align everything within main page

--- src/app/levelone/(pages)/abstract/page.tsx
+++ src/app/levelone/(pages)/abstract/page.tsx
@@ -3,17 +3,19 @@ import {
 	AbstractFooter,
 	AbstractHeader,
 	AbstractHeroSearch,
+	AbstractGrid,
 } from './_components_';
 
 const AbstractHomepage = () => {
 	return (
 		<div className={publicsans.className}>
 			<AbstractHeader />
-			<main>
-				<div className='flex flex-col items-center py-32 bg-indigo-100 gap-8'>
+			<main className='flex flex-col items-center'>
+				<div className='flex flex-col items-center py-32 bg-indigo-100 gap-8 w-full'>
 					<h1 className='text-3xl sm:text-6xl font-medium'>How can we help?</h1>
 					<AbstractHeroSearch />
 				</div>
+				<AbstractGrid />
 			</main>
 			<AbstractFooter />
 		</div>
