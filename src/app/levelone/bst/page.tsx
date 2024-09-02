6df7b410d7763fed9d7881c01f10e2a6e4ff3529
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-01 20:56:36 -0700

    Import all the new server components
    Change hierarchy a little bit

--- src/app/levelone/bst/page.tsx
+++ src/app/levelone/bst/page.tsx
@@ -1,48 +1,22 @@
 import { BSTContentElements } from './_components/Content';
 import BSTMain from './_components/BSTMain';
+import { BSTScrollProvider } from './_components/BSTScroll';
+import BSTHeader from './_components/BSTHeader';
+import BSTFooter from './_components/BSTFooter';
 
 const BSTHomepage = () => {
 	return (
-		<>
-			<header className='fixed z-10'>
-				<h1>Backstage Talks Clone</h1>
-				<nav>
-					<ul>
-						<li>
-							<a href='#issue8'>Issue #8</a>
-						</li>
-						<li>
-							<a href='#issue7'>Issue #7</a>
-						</li>
-						<li>
-							<a href='#issue6'>Issue #6</a>
-						</li>
-						<li>
-							<a href='#issue5'>Issue #5</a>
-						</li>
-						<li>
-							<a href='#issue4'>Issue #4</a>
-						</li>
-						<li>
-							<a href='#issue3'>Issue #3</a>
-						</li>
-						<li>
-							<a href='#issue2'>Issue #2</a>
-						</li>
-						<li>
-							<a href='#issue1'>Issue #1</a>
-						</li>
-					</ul>
-				</nav>
-			</header>
+		<BSTScrollProvider>
 			<BSTMain>
-				<ul className='h-full overflow-hidden scroll-smooth'>
+				<BSTHeader />
+				<ul className='h-full sm:overflow-hidden scroll-smooth'>
 					{BSTContentElements.map((IssueComponent, index) => (
 						<IssueComponent key={`map${index}`} />
 					))}
 				</ul>
+				<BSTFooter />
 			</BSTMain>
-		</>
+		</BSTScrollProvider>
 	);
 };
 
