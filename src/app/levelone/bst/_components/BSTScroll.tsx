9bdc948ad69d392cfe4e6b05e9f38121f76995f5
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-01 20:53:14 -0700

    Pass screen and setScreen to consumers
    Attach wheel event listener to window in a useEffect for scroll interception

--- src/app/levelone/bst/_components/BSTScroll.tsx
+++ src/app/levelone/bst/_components/BSTScroll.tsx
@@ -1,29 +1,43 @@
 'use client';
 
-import { useThrottle } from '@/lib/hooks';
-import { createContext, PropsWithChildren, useEffect, useState } from 'react';
+import {
+	createContext,
+	Dispatch,
+	PropsWithChildren,
+	SetStateAction,
+	useEffect,
+	useState,
+} from 'react';
+import { BSTContentLength } from './Content';
 
-const BTSScrollContext = createContext(0);
+const BTSScrollContext = createContext<{
+	screen: number;
+	setScreen: Dispatch<SetStateAction<number>>;
+}>({
+	screen: 0,
+	setScreen: () => {},
+});
 
 export default BTSScrollContext;
 
 export const BSTScrollProvider = ({ children }: PropsWithChildren) => {
 	const [screen, setScreen] = useState(0);
-	const { ready, triggerThrottle } = useThrottle(1);
 
 	useEffect(() => {
-		const windowHandler = (e: Event) => {
-			if (!ready) return;
-
-			triggerThrottle();
+		const handler = (e: WheelEvent) => {
+			if (window.innerWidth <= 640) return;
+			if (e.deltaY > 0 && screen < BSTContentLength - 1) {
+				window.location.hash = `#issue${BSTContentLength - screen - 1}`;
+			} else if (e.deltaY < 0 && screen > 0) {
+				window.location.hash = `#issue${BSTContentLength - screen + 1}`;
+			}
 		};
-		window.addEventListener('scroll', windowHandler);
-
-		return () => window.removeEventListener('scroll', windowHandler);
-	}, [ready, triggerThrottle]);
+		window.addEventListener('wheel', handler);
+		return () => window.removeEventListener('wheel', handler);
+	}, [screen]);
 
 	return (
-		<BTSScrollContext.Provider value={screen}>
+		<BTSScrollContext.Provider value={{ screen, setScreen }}>
 			{children}
 		</BTSScrollContext.Provider>
 	);
