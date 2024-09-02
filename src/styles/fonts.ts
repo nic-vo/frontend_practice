e0b51b0ec073f81a87724f291b7065325d96530e
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-01 20:48:14 -0700

    Add Oswald for BST

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,4 +1,4 @@
-import { Outfit, Public_Sans } from 'next/font/google';
+import { Oswald, Outfit, Public_Sans } from 'next/font/google';
 
 const defaultOptions = {
 	preload: false,
@@ -15,6 +15,13 @@ export const outfit = Outfit({
 export const publicsans = Public_Sans({
 	weight: 'variable',
 	subsets: ['latin', 'latin-ext'],
+	preload: false,
+	fallback: ['ui-sans-serif', 'sans-serif', 'system-ui'],
+});
+
+export const oswald = Oswald({
+	weight: 'variable',
+	subsets: ['latin'],
 	preload: false,
 	fallback: ['ui-sans-serif', 'sans-serif', 'system-ui'],
 });
