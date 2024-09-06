3cca2f0026f87a88d4acea2a98fd1a02a6fffb77
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-05 21:49:26 -0700

    Fix footer mobile vs desktop layout
    Breakpoints for the position behavior
    Hover and focus behavior for links

--- src/app/levelone/bst/_components/BSTFooter.tsx
+++ src/app/levelone/bst/_components/BSTFooter.tsx
@@ -1,8 +1,8 @@
 import { SafeExternalLink } from '@/components/global/Commons';
 
 const BSTFooter = () => (
-	<footer className='md:fixed md:bottom-0 md:h-0 w-full '>
-		<div className='md:fixed bottom-4 left-4 font-bold w-64'>
+	<footer className='flex flex-col items-center gap-8 md:fixed md:bottom-0 md:h-0 w-full pb-16 md:pb-0'>
+		<div className='md:fixed md:bottom-4 md:left-4 font-bold w-11/12 md:w-64 flex flex-col items-center md:items-start'>
 			<p>
 				Backstage Talks is a magazine of casual, but in depth dialogues on
 				design and business. Our decisions shape and influence this complex
@@ -10,19 +10,21 @@ const BSTFooter = () => (
 			</p>
 			<p className='font-normal'>
 				&copy; 2024{' '}
-				<SafeExternalLink href='http://buromilk.com/'>
+				<SafeExternalLink
+					href='http://buromilk.com/'
+					className='hover:underline focus-visible:outline outline-black outline-offset-2'>
 					Published by Buro Milk
 				</SafeExternalLink>
 			</p>
 			<SafeExternalLink
 				href='https://backstagetalks.com/privacy-policy.php'
-				className='block mt-8'>
+				className='block mt-8 w-max hover:underline focus-visible:outline outline-black outline-offset-2'>
 				Privacy Policy
 			</SafeExternalLink>
 		</div>
 		<SafeExternalLink
 			href='mailto:info@backstagetalks.com'
-			className='md:fixed md:bottom-4 md:right-4 font-bold'>
+			className='md:fixed md:bottom-4 md:right-4 font-bold block w-max text-center hover:underline focus-visible:outline outline-black outline-offset-2'>
 			info@backstagetalks.com
 		</SafeExternalLink>
 	</footer>
