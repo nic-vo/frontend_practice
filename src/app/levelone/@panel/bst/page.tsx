60edf8f358287f1c4900f5abeb6c83c40049d88c
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-07 13:24:47 -0700

    Flesh out BST panel with links and title

--- src/app/levelone/@panel/bst/page.tsx
+++ src/app/levelone/@panel/bst/page.tsx
@@ -1,4 +1,10 @@
-import { InfoTitle, PanelContentWrapper } from '@/components/panel';
+import {
+	InfoTitle,
+	LayoutNav,
+	OriginalNav,
+	PanelContentWrapper,
+	PanelNavList,
+} from '@/components/panel';
 
 const BSTPanel = () => (
 	<>
@@ -9,22 +15,30 @@ const BSTPanel = () => (
 				crucial, and I hope it eliminates the need for rate throttling a scroll
 				event listener. I had a few challenges, however:
 			</p>
-			<ol>
+			<ol className='flex flex-col gap-2 list-decimal pl-6'>
 				<li>
-					Setting up the breakpoints at which the scroll behavior changes seems
-					a little fragile because it involves setting a magic number that
-					aligns with Tailwinds breakpoints across multiple files. Perhaps
-					there&apos;s a better way to do this globally via a <pre>const</pre>{' '}
-					somewhere.
+					<p>
+						Setting up the breakpoints at which the scroll behavior changes
+						seems a little fragile because it involves setting a magic number
+						that aligns with Tailwinds breakpoints across multiple files.
+						Perhaps there&apos;s a better way to do this globally via a{' '}
+						<pre className='inline'>const</pre> somewhere.
+					</p>
 				</li>
 				<li>
-					Because the scroll behavior messes with the history API, the behavior
-					when moving between screen sizes can get a little weird with reseting
-					the scroll position and the like.
+					<p>
+						Because the scroll behavior messes with the history API, the
+						behavior when moving between screen sizes can get a little weird
+						with reseting the scroll position and the like.
+					</p>
 				</li>
 			</ol>
 			<p>All in all, I&apos;m happy with how this one turned out.</p>
 		</PanelContentWrapper>
+		<PanelNavList>
+			<OriginalNav original='https://backstagetalks.com/' />
+			<LayoutNav upOne='/levelone/' />
+		</PanelNavList>
 	</>
 );
 
