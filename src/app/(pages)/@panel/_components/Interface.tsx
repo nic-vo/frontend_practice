4d8f69ed44fa5df28935aa448f9ead402958f28b
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-09 12:25:47 -0700

    Return focus to panel toggle on panel close
    On subsequent renders via first useState

--- src/app/(pages)/@panel/_components/Interface.tsx
+++ src/app/(pages)/@panel/_components/Interface.tsx
@@ -10,9 +10,11 @@ import {
 	SetStateAction,
 	useCallback,
 	useContext,
+	useEffect,
+	useRef,
 	useState,
 } from 'react';
-import { IoHome, IoInformation, IoList } from 'react-icons/io5';
+import { IoClose, IoHome, IoInformation, IoList } from 'react-icons/io5';
 import { twMerge } from 'tailwind-merge';
 
 export const ToggleContext = createContext<{
@@ -68,16 +70,33 @@ const MainPanelNav = () => {
 	);
 };
 
-const MainPanel = ({ children }: PropsWithChildren) => {
+const PanelAside = ({ children }: PropsWithChildren) => {
 	const segments = usePathname().split('/').slice(1);
 	const { toggled, setToggled } = useContext(ToggleContext);
+	const buttonRef = useRef<HTMLButtonElement>(null);
+	const [first, setFirst] = useState(true);
+
+	useEffect(() => setFirst(false), []);
+
+	useEffect(() => {
+		if (first || toggled || !buttonRef.current) return;
+		buttonRef.current.focus();
+	}, [toggled]);
+
+	if (segments.length < 3) return null;
 	return (
 		<aside
 			className={twMerge([
-				'fixed md:right-0 md:h-lvh',
+				'fixed h-lvh sm:h-max w-lvw sm:w-min sm:max-w-96',
 				toggled ? 'translate-x-0' : 'translate-x-full',
-			])}>
-			<menu className='absolute bottom-4 -translate-x-[calc(100%+1rem)]'>
+			])}
+			aria-hidden={!toggled}
+			id='info-panel'>
+			<menu
+				className={twMerge([
+					'absolute bottom-4 -translate-x-[calc(100%+1rem)]',
+					toggled ? 'hidden sm:block' : 'block',
+				])}>
 				<ul>
 					<li>
 						<Link href='/'>
@@ -86,13 +105,17 @@ const MainPanel = ({ children }: PropsWithChildren) => {
 						</Link>
 					</li>
 					<li>
-						<Link href={segments[0]}>
+						<Link href={'/' + segments[1]}>
 							<IoList aria-hidden />
 							<span className='sr-only'>Return to index</span>
 						</Link>
 					</li>
 					<li>
-						<button onClick={() => setToggled((prev) => !prev)}>
+						<button
+							onClick={() => setToggled((prev) => !prev)}
+							aria-expanded={toggled}
+							aria-controls='info-panel'
+							ref={buttonRef}>
 							<IoInformation aria-hidden />
 							<span className='sr-only'>Toggle info panel</span>
 						</button>
@@ -100,7 +123,19 @@ const MainPanel = ({ children }: PropsWithChildren) => {
 				</ul>
 			</menu>
 			{children}
+			<button
+				onClick={() => setToggled(false)}
+				tabIndex={toggled ? 0 : -1}>
+				<IoClose aria-hidden />
+				<span className='sr-only'>Close info panel</span>
+			</button>
 			<MainPanelNav />
 		</aside>
 	);
 };
+
+const MainPanelComponent = ({ children }: PropsWithChildren) => (
+	<ToggleContextProvider>
+		<PanelAside>{children}</PanelAside>
+	</ToggleContextProvider>
+);
