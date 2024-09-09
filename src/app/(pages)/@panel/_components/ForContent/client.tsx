4825abe9154790d56a08642b9b78a6842824550f
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-09 12:16:26 -0700

    Split ForContent into client and server

---
+++ src/app/(pages)/@panel/_components/ForContent/client.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { SafeExternalLink } from '@/components/global/Commons';
+import { ToggleContext } from '../Interface';
+import { useContext } from 'react';
+
+export const PanelOriginalLink = ({
+	children,
+	href,
+}: {
+	href: string;
+	children: React.ReactNode;
+}) => {
+	const { toggled } = useContext(ToggleContext);
+
+	return (
+		<SafeExternalLink
+			href={href}
+			aria-hidden={!toggled}
+			tabIndex={toggled ? 0 : -1}>
+			{children}
+		</SafeExternalLink>
+	);
+};
