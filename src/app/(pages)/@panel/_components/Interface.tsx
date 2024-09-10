9e15d3e8973781da0d3f9e7aec0e9af4d13af987
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-10 01:07:14 -0700

    Style aside panel
    Remove blur handler from last panel link
    Add another close panel button instead

--- src/app/(pages)/@panel/_components/Interface.tsx
+++ src/app/(pages)/@panel/_components/Interface.tsx
@@ -5,10 +5,8 @@ import { usePathname } from 'next/navigation';
 import {
 	createContext,
 	Dispatch,
-	FocusEventHandler,
 	PropsWithChildren,
 	SetStateAction,
-	useCallback,
 	useContext,
 	useEffect,
 	useRef,
@@ -36,16 +34,8 @@ const MainPanelNav = () => {
 	const linkRef = useRef<HTMLAnchorElement>(null);
 	const { toggled, setToggled } = useContext(ToggleContext);
 
-	const blurHandler: FocusEventHandler<HTMLAnchorElement> = useCallback(
-		(e) => {
-			if (!linkRef.current || e.relatedTarget === linkRef.current) return;
-			setToggled(false);
-		},
-		[setToggled],
-	);
-
 	return (
-		<nav>
+		<menu>
 			<ul>
 				<li>
 					<Link
@@ -60,15 +50,25 @@ const MainPanelNav = () => {
 					<Link
 						href={'/' + segment}
 						tabIndex={toggled ? 0 : -1}
-						aria-hidden={!toggled}
-						onBlur={blurHandler}>
+						aria-hidden={!toggled}>
 						Return to index
 					</Link>
 				</li>
+				<li>
+					<button
+						onClick={() => setToggled(false)}
+						aria-hidden={!toggled}
+						tabIndex={toggled ? 0 : -1}>
+						Close info panel
+					</button>
+				</li>
 			</ul>
-		</nav>
+		</menu>
 	);
 };
+
+const MainPanelNavClasser =
+	'block text-xl bg-black text-white p-2 rounded-full origin-right hover:scale-110 transition-all hover:bg-rose-700 focus-visible:bg-rose-700 focus-visible:outline outline-rose-700 outline-offset-2';
 
 const PanelAside = ({ children }: PropsWithChildren) => {
 	const segments = usePathname().split('/').slice(1);
@@ -83,29 +83,33 @@ const PanelAside = ({ children }: PropsWithChildren) => {
 		buttonRef.current.focus();
 	}, [toggled]);
 
-	if (segments.length < 3) return null;
+	if (segments.length < 2) return null;
 	return (
 		<aside
 			className={twMerge([
-				'fixed h-lvh sm:h-max w-lvw sm:w-min sm:max-w-96',
+				'top-0 right-0 fixed z-20 h-lvh sm:h-max w-lvw sm:max-w-lg transition-all p-6 bg-black text-white',
 				toggled ? 'translate-x-0' : 'translate-x-full',
 			])}
 			aria-hidden={!toggled}
 			id='info-panel'>
 			<menu
 				className={twMerge([
-					'absolute bottom-4 -translate-x-[calc(100%+1rem)]',
+					'absolute top-4 left-0 -translate-x-full p-2',
 					toggled ? 'hidden sm:block' : 'block',
 				])}>
-				<ul>
+				<ul className='flex flex-col gap-4 *:block'>
 					<li>
-						<Link href='/'>
+						<Link
+							href='/'
+							className={MainPanelNavClasser}>
 							<IoHome aria-hidden />
 							<span className='sr-only'>Return home</span>
 						</Link>
 					</li>
 					<li>
-						<Link href={'/' + segments[1]}>
+						<Link
+							href={'/' + segments[0]}
+							className={MainPanelNavClasser}>
 							<IoList aria-hidden />
 							<span className='sr-only'>Return to index</span>
 						</Link>
@@ -115,7 +119,8 @@ const PanelAside = ({ children }: PropsWithChildren) => {
 							onClick={() => setToggled((prev) => !prev)}
 							aria-expanded={toggled}
 							aria-controls='info-panel'
-							ref={buttonRef}>
+							ref={buttonRef}
+							className={MainPanelNavClasser}>
 							<IoInformation aria-hidden />
 							<span className='sr-only'>Toggle info panel</span>
 						</button>
@@ -125,7 +130,8 @@ const PanelAside = ({ children }: PropsWithChildren) => {
 			{children}
 			<button
 				onClick={() => setToggled(false)}
-				tabIndex={toggled ? 0 : -1}>
+				tabIndex={toggled ? 0 : -1}
+				className='absolute top-2 right-2 p-1 border-2 border-white rounded-full hover:bg-white hover:text-black focus-visible:bg-white focus-visible:text-black transition-all outline-none'>
 				<IoClose aria-hidden />
 				<span className='sr-only'>Close info panel</span>
 			</button>
@@ -139,3 +145,5 @@ const MainPanelComponent = ({ children }: PropsWithChildren) => (
 		<PanelAside>{children}</PanelAside>
 	</ToggleContextProvider>
 );
+
+export default MainPanelComponent;
