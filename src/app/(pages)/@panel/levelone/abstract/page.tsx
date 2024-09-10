2ea8ca4df6977ba0fd0aa10c30de3572ce5ca1e0
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-10 01:08:17 -0700

    Update all panel pages to use new components

--- src/app/(pages)/@panel/levelone/abstract/page.tsx
+++ src/app/(pages)/@panel/levelone/abstract/page.tsx
@@ -1,32 +1,26 @@
 import {
-	InfoTitle,
-	LayoutNav,
-	OriginalNav,
+	PanelContentLink,
+	PanelContentTitle,
 	PanelContentWrapper,
-	PanelNavList,
-} from '@/app/(pages)/@panel/_components';
+} from '../../_components/ForContent';
 
 const AbstractPanel = () => {
 	return (
-		<>
-			<InfoTitle title='Abstract Help Page Clone' />
-			<PanelContentWrapper>
-				<p>
-					This was also a quick &apos;n easy clone to pull off. No crazy
-					JavaScript. Getting better at finding the sweet spot of abstraction in
-					terms of components, too.
-				</p>
-				<p>
-					The original footer layout is really weird, though. I took a few
-					liberties in terms of breakpoints and flexbox to keep it sane and
-					neat.
-				</p>
-			</PanelContentWrapper>
-			<PanelNavList>
-				<OriginalNav original='https://help.abstract.com/hc/en-us' />
-				<LayoutNav upOne='/levelone/' />
-			</PanelNavList>
-		</>
+		<PanelContentWrapper>
+			<PanelContentTitle>Abstract Help Page Clone</PanelContentTitle>
+			<p>
+				This was also a quick &apos;n easy clone to pull off. No crazy
+				JavaScript. Getting better at finding the sweet spot of abstraction in
+				terms of components, too.
+			</p>
+			<p>
+				The original footer layout is really weird, though. I took a few
+				liberties in terms of breakpoints and flexbox to keep it sane and neat.
+			</p>
+			<PanelContentLink href='https://help.abstract.com/hc/en-us'>
+				Original
+			</PanelContentLink>
+		</PanelContentWrapper>
 	);
 };
 
