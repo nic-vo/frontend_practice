662b4aade26018d099ad0f6e9a5abef054aeae77
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-09-17 00:19:04 -0700

    Style the hover and focus behavior for main <nav> tags

--- src/app/(pages)/levelone/ohstudio/_components/OHNav/index.tsx
+++ src/app/(pages)/levelone/ohstudio/_components/OHNav/index.tsx
@@ -12,7 +12,7 @@ import Image from 'next/image';
 
 import type { StaticImageData } from 'next/image';
 import type { PropsWithChildren } from 'react';
-import { IoHeartOutline } from 'react-icons/io5';
+import { IoArrowUp, IoHeartOutline, IoLockClosed } from 'react-icons/io5';
 
 const commonUrl = 'https://oh.studio/projects/';
 
@@ -22,7 +22,7 @@ const CommonLink = ({
 }: PropsWithChildren & { projectName: string }) => (
 	<SafeExternalLink
 		href={projectName !== '' ? commonUrl + projectName : ''}
-		className='block'>
+		className='block group/link before:hidden sm:before:block before:content[""] before:absolute before:h-full before:w-full sm:rounded-2xl sm:overflow-hidden before:transition-all hover:before:backdrop-blur hover:before:backdrop-brightness-75 focus-visible:before:backdrop-blur focus-visible:before:backdrop-brightness-75 before:z-10 focus-visible:outline-2 outline-black outline-offset-2'>
 		{children}
 	</SafeExternalLink>
 );
@@ -31,9 +31,33 @@ const CommonImage = ({ data }: { data: StaticImageData }) => (
 	<Image
 		src={data}
 		alt=''
-		className='aspect-[4/3] overflow-hidden rounded-2xl'
+		className='aspect-[4/3] overflow-hidden rounded-2xl sm:rounded-none z-0'
 		style={{ objectFit: 'cover' }}
 	/>
+);
+
+const LinkText = ({ children }: PropsWithChildren) => (
+	<p className='sm:absolute sm:top-4 sm:left-4 sm:opacity-0 sm:group-hover/link:opacity-100 sm:group-focus-visible/link:opacity-100 transition-opacity sm:text-white z-20'>
+		{children}
+	</p>
+);
+
+const LinkArrow = () => (
+	<div
+		className='hidden sm:block absolute top-4 right-4 rotate-45 text-xl text-black rounded-full z-20 p-2 bg-neutral-200 opacity-0 -translate-x-full translate-y-full sm:group-hover/link:opacity-100 sm:group-hover/link:translate-x-0 sm:group-hover/link:translate-y-0 sm:group-focus-visible/link:opacity-100 sm:group-focus-visible/link:translate-x-0 sm:group-focus-visible/link:translate-y-0 transition-all'
+		role='image'
+		aria-label='Arrow icon'>
+		<IoArrowUp aria-hidden />
+	</div>
+);
+
+const LinkLock = () => (
+	<div
+		className='block absolute top-4 right-4 text-xl text-black rounded-full z-20 p-2 bg-neutral-200 sm:opacity-0 sm:-translate-x-full sm:translate-y-full sm:group-hover/link:opacity-100 sm:group-hover/link:translate-x-0 sm:group-hover/link:translate-y-0 sm:group-focus-visible/link:opacity-100 sm:group-focus-visible/link:translate-x-0 sm:group-focus-visible/link:translate-y-0 transition-all'
+		role='image'
+		aria-label='Arrow icon'>
+		<IoLockClosed aria-hidden />
+	</div>
 );
 
 const LinkArr = [
@@ -41,43 +65,50 @@ const LinkArr = [
 		key='aire'
 		projectName='aire'>
 		<CommonImage data={Perfume} />
-		<p>Aire</p>
+		<LinkText>Aire</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='correlated'
 		projectName='correlated'>
 		<CommonImage data={iPad} />
-		<p>Correlated</p>
+		<LinkText>Correlated</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='my5'
 		projectName='my5'>
 		<CommonImage data={iPad} />
-		<p>Channel 5</p>
+		<LinkText>Channel 5</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='ronald-abram'
 		projectName='ronald-abram'>
 		<CommonImage data={Jewelry} />
-		<p>Ronald Abram</p>
+		<LinkText>Ronald Abram</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='propeller'
 		projectName='propeller'>
 		<CommonImage data={BusStop} />
-		<p>Propeller</p>
+		<LinkText>Propeller</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='schuh'
 		projectName='schuh'>
 		<CommonImage data={iPhone} />
-		<p>Schuh</p>
+		<LinkText>Schuh</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='paperstreet'
 		projectName='paperstreet'>
 		<CommonImage data={MacBook} />
-		<p>Paperstreet</p>
+		<LinkText>Paperstreet</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='oh-supply'
@@ -91,36 +122,45 @@ const LinkArr = [
 				aria-hidden
 			/>
 		</div>
-		<p>OH.SUPPLY</p>
+		<LinkText>OH.SUPPLY</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='monokel-eyewear'
 		projectName='monokel-eyewear'>
 		<CommonImage data={iPad} />
-		<p>Monokel Eyewear</p>
+		<LinkText>Monokel Eyewear</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 	<CommonLink
 		key='lawtrades'
 		projectName=''>
 		<CommonImage data={iPhone} />
-		<p>Lawtrades</p>
+		<LinkText>
+			Lawtrades <span className='text-neutral-400'>Coming soon</span>
+		</LinkText>
+		<LinkLock />
 	</CommonLink>,
 	<CommonLink
 		key='baselworld'
 		projectName=''>
 		<CommonImage data={iPhone} />
-		<p>Baselworld</p>
+		<LinkText>
+			Baselworld <span className='text-neutral-400'>Coming soon</span>
+		</LinkText>
+		<LinkLock />
 	</CommonLink>,
 	<CommonLink
-		key=''
+		key='nycb'
 		projectName='nycb'>
 		<CommonImage data={Dancers} />
-		<p>NYCB</p>
+		<LinkText>NYCB</LinkText>
+		<LinkArrow />
 	</CommonLink>,
 ];
 
 const OHNav = () => (
-	<nav className='w-11/12 max-w-screen-xl'>
+	<nav className='w-11/12 max-w-screen-xl z-0 mt-32'>
 		<ul className='grid grid-cols-1 md:grid-cols-2 gap-4'>
 			{LinkArr.map((Thing, index) => (
 				<li
